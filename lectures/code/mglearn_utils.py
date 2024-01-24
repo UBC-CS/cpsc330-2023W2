ff7c41a43a0e75954f48c5c6df20111e28289d63
Mathias <mathias@lecuyer.me>
2024-01-24 15:04:44 -0800

    .

--- lectures/code/mglearn_utils.py
+++ lectures/code/mglearn_utils.py
@@ -3,7 +3,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap, colorConverter
 from sklearn.datasets import make_blobs
-
+from sklearn.preprocessing import (StandardScaler, MinMaxScaler, Normalizer,
+                                   RobustScaler)
 cm3 = ListedColormap(['#0000aa', '#ff2020', '#50ff50'])
 cm2 = ListedColormap(['#0000aa', '#ff2020'])
 
@@ -314,3 +315,38 @@ def make_wave(n_samples=100):
     y_no_noise = (np.sin(4 * x) + x)
     y = (y_no_noise + rnd.normal(size=len(x))) / 2
     return x.reshape(-1, 1), y
+
+def plot_scaling():
+    X, y = make_blobs(n_samples=50, centers=2, random_state=4, cluster_std=1)
+    X += 3
+
+    plt.figure(figsize=(15, 8))
+    main_ax = plt.subplot2grid((2, 4), (0, 0), rowspan=2, colspan=2)
+
+    main_ax.scatter(X[:, 0], X[:, 1], c=y, cmap=cm2, s=60)
+    maxx = np.abs(X[:, 0]).max()
+    maxy = np.abs(X[:, 1]).max()
+
+    main_ax.set_xlim(-maxx + 1, maxx + 1)
+    main_ax.set_ylim(-maxy + 1, maxy + 1)
+    main_ax.set_title("Original Data")
+    other_axes = [plt.subplot2grid((2, 4), (i, j))
+                  for j in range(2, 4) for i in range(2)]
+
+    for ax, scaler in zip(other_axes, [StandardScaler(), RobustScaler(),
+                                       MinMaxScaler(), Normalizer(norm='l2')]):
+        X_ = scaler.fit_transform(X)
+        ax.scatter(X_[:, 0], X_[:, 1], c=y, cmap=cm2, s=60)
+        ax.set_xlim(-2, 2)
+        ax.set_ylim(-2, 2)
+        ax.set_title(type(scaler).__name__)
+
+    other_axes.append(main_ax)
+
+    for ax in other_axes:
+        ax.spines['left'].set_position('center')
+        ax.spines['right'].set_color('none')
+        ax.spines['bottom'].set_position('center')
+        ax.spines['top'].set_color('none')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')
